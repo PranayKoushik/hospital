0c21b982a3dcc93ff6234283a2729eb62b13ede3
lokesh <tadurilokesh15@gmail.com>
2025-11-27 00:03:52 +0530

    changed

--- src/main/java/com/alpha/hospital/service/PatientService.java
+++ src/main/java/com/alpha/hospital/service/PatientService.java
@@ -1,5 +1,19 @@
 package com.alpha.hospital.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alpha.hospital.Repository.PatientRepo;
+import com.alpha.hospital.entity.Patient;
+
 public class PatientService {
+	
+	@Autowired
+	private PatientRepo pr;
+
+	public void saveStu(Patient p) {
+		pr.save(p);
+		// TODO Auto-generated method stub
+		
+	}
 
 }
